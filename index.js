aa8fa8dce5afbaf19bed26c155de096d77a81e47
jeonhyoungmin <jhmin7509@gamil.com>
2022-09-08 00:55:53 +0900

    삼항 연산자 연속해서 사용 가능하지만 가해성이 떨어질 수 있음

---
+++ index.js
@@ -0,0 +1,16 @@
+let operandOne = "test";
+let operandTwo = 4;
+// operand(용어): 피연산자
+let result = (operandOne.length === operandTwo) ? "hello" : "bye";
+
+console.log(result);
+
+let example = 2
+let exampleTwo = "안녕"
+
+let resultTwo = (operandOne.length === operandTwo) ? 
+(example === exampleTwo.length) ? "true" : "false" 
+: "bye"
+
+console.log(resultTwo)
+console.log(exampleTwo.length)
